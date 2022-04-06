c2cebf2a2ab641a4da8914498bd915f572f5d999
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-04-06 14:23:05 +0500

    initial commit

---
+++ js/exercise.js
@@ -0,0 +1,83 @@
+// 1-MASALA 
+
+
+// let arr = [10,1,3,5,8,9];
+// for (let i = 0; i < arr.length; i++){
+//    if (arr[i] > arr[i+1]){
+//      let result = arr[i+1];
+//      arr[i+1] = arr[i];
+//      arr[i] = result;
+//    }
+// }
+// console.log(arr);
+
+
+
+// 2-Masala 
+
+// let obj = [
+//   {
+//     login : "forever",
+//     pass: "forever123"
+//   }
+// ];
+
+// obj.forEach((item) => {
+//   if(item.login.trim().length < 3 && item.pass.trim().length < 8){
+  
+//     console.log("Login kamida 3 belgidan iborat bo'lishi kerak");
+//     console.log("Parol kamida 8 belgidan iborat bo'lishi kerak");
+//   }
+  
+//   else if(item.login.trim().length < 3){
+//     console.log("Login kamida 3 belgidan iborat bo'lishi kerak");
+//   }
+//    else if(item.pass.trim().length < 8){
+//     console.log("Parol kamida 8 belgidan iborat bo'lishi kerak");
+//   } 
+//   else{
+//     console.log("Siz muvaffaqqiyatli ro'yxatdan o'tdingiz");
+//   }
+// })
+
+
+// 4-Masala
+
+// let n = 2;
+// let k = 3;
+// let result = 0;
+
+// for(let i = 1; i <= n; i++){
+//   if(i > 1){
+//     let pow = 1;
+//     for(let j = 0; j < k; j++){
+//       pow *= i;
+//     }
+//     result += pow;
+//   } else if(i == 1){
+//     result += i;
+//   }
+// }
+
+// console.log(result);
+
+
+// 6-Masala 
+
+// let arr = [1,2,3,4];
+// let half = arr.length/2;
+// let result = half;
+
+// for (let i = 0; i < half; i++){
+//   let c = arr[i];
+//   arr[i] = arr[result];
+//   arr[result] = c;
+//   result++;
+  
+// }
+// console.log(arr);
+
+
+// 5-Masala 
+
+
---
+++ js/main.js
@@ -0,0 +1,34 @@
+const right = document.querySelector('.right-btn');
+const left = document.querySelector('.left-btn');
+const lock = document.querySelector('.lock-icon');
+const eye = document.querySelector('.eye-icon');
+const input = document.querySelector('.input');
+const list = document.querySelector('.list');
+const form = document.querySelector('.form');
+const lastLi = document.querySelector('.last-li');
+const symbol = document.querySelector('.character');
+
+right.addEventListener('click', ()=>{
+  right.innerHTML = `<i class='bx bxs-bullseye'></i>`;
+  input.style.backgroundColor = '#fff';
+  input.style.transition = 'all 0.4s ease';
+  input.style.color = '#000';
+  lock.style.color = '#121327';
+})
+
+
+
+form.addEventListener('keydown', ()=>{
+  let val = input.value;
+  let symbol = '@';
+  list.style.opacity = '1';
+  let text = lastLi.textContent
+  if (val.length > 8){
+    lastLi.innerHTML = `<i class='bx bx-check' ></i>More than 8 character`;
+  } 
+   if (val.includes(symbol)){
+     symbol.innerHTML = `<i class='bx bx-check' ></i>Special Character`;
+  }
+})
+
+
